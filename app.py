6b68d5e55eff94cb6662545717dbb4598eaa16db
Yu Min Kong <yuminkong@Mins-MacBook-Air.local>
2025-01-25 19:29:41 +0000

    Update project with requirements.txt and production setup

--- app.py
+++ app.py
@@ -16,7 +16,42 @@ def execute_code():
     code = data.get('code', '')
 
     # 黑名单检查，避免危险代码
-    forbidden_keywords = ['import os', 'import sys', 'subprocess', '__import__', 'eval(', 'exec(']
+    forbidden_keywords = [
+    # 模块导入
+    'import os', 'import sys', 'import subprocess', 'import shutil',
+    'import socket', 'import multiprocessing', 'import threading', 'import asyncio',
+    'import logging', 'import pathlib', 'import pickle', 'import base64',
+    'import ctypes', 'import pwd', 'import grp',
+
+    # 内置危险函数
+    '__import__', 'eval(', 'exec(', 'compile(', 'execfile(', 'input(', 'globals(',
+    'locals(', 'vars(', 'delattr(', 'setattr(', 'getattr(', 'open(', 'file(', 
+    'os.system', 'os.popen', 'os.execl', 'os.execle', 'os.execlp', 'os.execvp',
+    'os.fork', 'os.spawn', 'os.remove', 'os.unlink', 'os.rmdir', 'os.makedirs',
+    'os.chmod', 'os.chown', 'os.rename', 'os.kill', 'os.abort',
+
+    # subprocess and shell
+    'subprocess.Popen', 'subprocess.run', 'subprocess.call', 'subprocess.check_output',
+    'subprocess.check_call', 'shlex.split', 'shutil.rmtree', 'shutil.copy', 'shutil.move',
+
+    # 网络和套接字
+    'socket.socket', 'socket.bind', 'socket.connect', 'socket.listen',
+    'socket.accept', 'socket.recv', 'socket.send', 'http.client', 'urllib',
+
+    # 多线程/多进程
+    'threading.Thread', 'multiprocessing.Process', 'multiprocessing.Pool',
+
+    # 数据操作
+    'pickle.load', 'pickle.loads', 'base64.b64decode', 'marshal.loads', 'marshal.load',
+
+    # 文件操作
+    'open(', 'write(', 'read(', 'delete(', 'save(', 'chmod(', 'chown(', 'unlink(',
+
+    # 其他危险函数
+    'ctypes.', 'getattr(', 'setattr(', 'delattr(', 'globals()', 'locals()',
+
+    'import turtle',
+]
     if any(keyword in code for keyword in forbidden_keywords):
         return jsonify({"error": "Forbidden keyword in code"}), 403
 
